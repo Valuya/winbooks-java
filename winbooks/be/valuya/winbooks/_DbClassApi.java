41c9e358e5556f4545fc314a64c335f47b7f594f
Yannick Majoros <yannick@valuya.be>
2013-10-26 12:09:47 +0200

    removed useless stuff

--- winbooks/be/valuya/winbooks/_DbClassApi.java
+++
@@ -1,9 +0,0 @@
-package be.valuya.winbooks  ;
-
-import com4j.*;
-
-@IID("{0BC7D7D2-BAB4-4DDD-AF35-258AEE3C6213}")
-public interface _DbClassApi extends Com4jObject {
-  // Methods:
-  // Properties:
-}
