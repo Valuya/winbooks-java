3972bee8da4d355c8d45e5479fb6a5cbbe5c4cca
cghislai <charlyghislain@gmail.com>
2019-07-08 09:23:08 +0200

    Archived book years

--- winbooks-api-extra/src/main/java/be/valuya/winbooks/api/accountingtroll/converter/ATBookYearConverter.java
+++ winbooks-api-extra/src/main/java/be/valuya/winbooks/api/accountingtroll/converter/ATBookYearConverter.java
@@ -2,6 +2,7 @@ package be.valuya.winbooks.api.accountingtroll.converter;
 
 import be.valuya.accountingtroll.domain.ATBookYear;
 import be.valuya.jbooks.model.WbBookYearFull;
+import be.valuya.jbooks.model.WbBookYearStatus;
 
 import java.time.LocalDate;
 
@@ -11,11 +12,13 @@ public class ATBookYearConverter {
         String shortName = wbYear.getShortName();
         LocalDate startDate = wbYear.getStartDate();
         LocalDate endDate = wbYear.getEndDate();
+        boolean archived = wbYear.getArchivePathNameOptional().isPresent();
 
         ATBookYear bookYear = new ATBookYear();
         bookYear.setName(shortName);
         bookYear.setStartDate(startDate);
         bookYear.setEndDate(endDate);
+        bookYear.setArchived(archived);
         return bookYear;
     }
 }
